5502716b2b6e66ecc04bc4236d877c76947b020d
VivekSetu <vivek@setuconsulting.com>
2023-01-09 12:11:42 +0530

    log

--- build.sh
+++ build.sh
@@ -1,3 +1,2 @@
-cp ./remote_syslog /usr/local/bin
+./remote_syslog   -p 32860 --tls   -d logs2.papertrailapp.com   --pid-file=/var/run/remote_syslog.pid   /tmp/file.log
 pip install -r requirements.txt
-remote_syslog   -p 32860 --tls   -d logs2.papertrailapp.com   --pid-file=/var/run/remote_syslog.pid   /tmp/file.log
